53b591b1f7fcea62dac1d555f3703b9f8a78998e
Shumpei Nagata <nagata.syunpei@gmail.com>
2026-01-19 20:21:40 +0900

    プロジェクト作成

---
+++ Sandbox-MultiModule-iOSApp/Sandbox-MultiModule-iOSApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sandbox-MultiModule-iOSApp
+//
+//  Created by Shumpei Nagata on 2026/01/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sandbox-MultiModule-iOSApp/Sandbox-MultiModule-iOSApp/Sandbox_MultiModule_iOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sandbox_MultiModule_iOSAppApp.swift
+//  Sandbox-MultiModule-iOSApp
+//
+//  Created by Shumpei Nagata on 2026/01/19.
+//
+
+import SwiftUI
+
+@main
+struct Sandbox_MultiModule_iOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
